7073ff6ba6998e2028dbdba04de0c8e44fe373cb
martinxcalderon <martincalde98@gmail.com>
2023-03-28 12:34:43 +0200

    Commit

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package hola.mundo;
+
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo  ");
+        // TODO code application logic here
+    }
+    
+}
